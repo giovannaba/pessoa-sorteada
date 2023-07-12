af2d4ad6d1874c1942319984b4cfb407d369fc55
Giovanna Andrade <130957052+giovannaba@users.noreply.github.com>
2023-07-12 15:32:51 -0300

    solicitando ao usuário para incluir nomes na lista

---
+++ lista-pessoa-sorteada.py
@@ -0,0 +1,5 @@
+import random
+
+lista = ["Maria", "Jose", "Antonio", "Estrela", "Marcos", "Ingrid", "Vitoria"]
+escolhido = random.choice(lista)
+print('A pessoa sorteada foi: {}'.format(escolhido))
--- pessoa-sorteada.py
+++ pessoa-sorteada.py
@@ -1,5 +1,9 @@
 import random
 
-lista = ["Maria", "Jose", "Antonio", "Estrela", "Marcos", "Ingrid", "Vitoria"]
+qtdPessoas = int(input('Quantas pessoas irá participar desse sorteio: '))
+lista = []
+for c in range (1, qtdPessoas+1):
+    nomes = str(input('Informe o nome da pessoa para incluí-lá no sorteio: '))
+    lista.append(nomes)
 escolhido = random.choice(lista)
-print('A pessoa sorteada foi: {}'.format(escolhido))
+print('A pessoa escolhida foi: {}'.format(escolhido))
